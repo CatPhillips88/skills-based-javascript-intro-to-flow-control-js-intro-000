89e6aaea1f63efca79a5e68bb293b8b06dadbfa2
Catherine Phillips <catherineep4189@gmail.com>
2019-06-21 22:25:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if(ge >= 13 && age <= 19) {
+  if(age >= 13 && age <= 19) {
     return "You are a teenager!"
   } else {
     return "You are not a teenager"
@@ -13,7 +13,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if(ge >= 13 && age <= 19) {
+  if(age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else if(age <= 12) {
     return "You are a kid";
