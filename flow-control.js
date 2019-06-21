a020a3d365358a3290ed7d46912c6e90e871b120
Catherine Phillips <catherineep4189@gmail.com>
2019-06-21 22:19:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -13,12 +13,12 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if(age >= 12 && age <=19) {
-    return "You are a teenager!"
+  if(age >= 13 && age <=19) {
+    return "You are a teenager!";
   } else if(age <= 12) {
-    return "You are a kid"
+    return "You are a kid";
   } else {
-    return "You are a grownup"
+    return "You are a grownup";
   }
 }
 
@@ -29,19 +29,13 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
       case 13:
-        return "You are a teenager"
       case 14:
-        return "You are a teenager"
       case 15:
-        return "You are a teenager"
       case 16:
-        return "You are a teenager"
       case 17:
-        return "You are a teenager"
       case 18:
-        return "You are a teenager"
       case 19:
-        return "You are a teenager"
+        return "You are a teenager";
       default:
         return "You have an age"
 
